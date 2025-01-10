885550e71db6c76ee3f28222d955bcd8062b243c
Azmat-ALi786 <azmatali12.mghl@gmail.com>
2025-01-10 11:21:39 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+import mycaluclator1 as mc
+
+a = input("Enter first numbr for add : ")
+b = input("Enter second numbr for add : ")
+
+
+a = mc.SUM(a, b)
+
+print(a)
+
---
+++ mycaluclator1.py
@@ -0,0 +1,11 @@
+def SUM(a, b):
+    return a + b
+
+def substraction(a, b):
+    return a + b
+
+def Multiplye(a, b):
+    return a + b
+
+def division(a, b):
+    return a + b
